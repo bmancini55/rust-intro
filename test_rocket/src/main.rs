7919cbadf9b9fac75ea2e9ebb128a8d3aaedc3aa
Brian Mancini <bmancini@gmail.com>
2020-06-19 12:19:14 -0400

    initial creation of rocket example

---
+++ test_rocket/src/main.rs
@@ -0,0 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket;
+use rocket::*;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
