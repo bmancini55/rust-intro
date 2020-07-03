3b770061661280ac8ce80f901c35473334948902
Brian Mancini <bmancini@gmail.com>
2020-07-03 15:44:57 -0400

    rocket: adding basic streaming example from a file

--- test_rocket/src/main.rs
+++ test_rocket/src/main.rs
@@ -1,13 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use rocket;
+use rocket::response::Stream;
 use rocket::*;
+use std::fs::File;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/stream")]
+fn stream() -> Result<Stream<File>, std::io::Error> {
+    let f = File::open("test.txt")?;
+    let s = Stream::from(f);
+    Ok(s)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, stream]).launch();
 }
