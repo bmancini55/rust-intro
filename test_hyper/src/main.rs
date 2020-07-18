17205c1151f7cf8188ff919b6e135f681ae45866
Brian Mancini <bmancini@gmail.com>
2020-07-18 19:57:10 -0400

    getting the async stuff to work!

--- test_hyper/src/main.rs
+++ test_hyper/src/main.rs
@@ -3,7 +3,12 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use futures::StreamExt;
+use sqlx::mysql::*;
+use sqlx::prelude::*;
+
 use std::convert::Infallible;
+use std::env;
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -11,11 +16,24 @@ async fn main() {
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
+    // obtain the database url
+    let database_url = &env::var("DATABASE_URL").unwrap();
+
+    // Create a connection pool
+    let pool = MySqlPool::new(database_url).await.unwrap();
+
     // A `Service` is needed for every connection, so this
     // creates one from our `handle_conn` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(handle_request))
+    let make_svc = make_service_fn(move |_conn| {
+        let pool = pool.clone();
+
+        async move {
+            // service_fn converts our function into a `Service`
+            Ok::<_, hyper::Error>(service_fn(move |req| {
+                let pool = pool.clone();
+                handle_request(req, pool)
+            }))
+        }
     });
 
     let server = Server::bind(&addr).serve(make_svc);
@@ -26,7 +44,7 @@ async fn main() {
     }
 }
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle_request(req: Request<Body>, pool: MySqlPool) -> Result<Response<Body>, Infallible> {
     middleware_log(&req).await;
 
     match (req.method(), req.uri().path()) {
@@ -38,6 +56,8 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible
 
         // simple async method
         (&Method::GET, "/async") => run_async(req).await,
+
+        (&Method::GET, "/data") => run_data(req, pool).await,
 
         // 404
         _ => {
@@ -85,5 +105,33 @@ async fn run_async(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
                 .unwrap();
         }
     });
+    Ok(Response::new(body))
+}
+
+async fn run_data(_req: Request<Body>, pool: MySqlPool) -> Result<Response<Body>, Infallible> {
+    let (mut channel, body) = Body::channel();
+
+    // Spawn a new worker task which will execute our query task
+    // and will write to the producer channel!
+    tokio::spawn(async move {
+        // Map into user defined type
+        let mut stream = sqlx::query("select unix from candle.binance_btc_usdt")
+            .map(|row: sqlx::mysql::MySqlRow| {
+                let unix: i32 = row.get(0);
+                unix
+            })
+            .fetch(&pool);
+
+        // We need to use futures::StreamExto use the `next` method
+        let mut count: i32 = 0;
+        while let Some(record) = stream.next().await {
+            let data = format!("{}\n", record.unwrap());
+            channel.send_data(data.into()).await.unwrap();
+            std::thread::sleep(std::time::Duration::from_millis(1));
+            count += 1;
+            println!("row {}", count);
+        }
+    });
+
     Ok(Response::new(body))
 }
