fffdd76bf6b86556a0d1e869dc2ae160a0b9d5b1
Brian Mancini <bmancini@gmail.com>
2020-07-18 19:00:32 -0400

    adding sqlx streaming tests

--- test_sqlx/src/main.rs
+++ test_sqlx/src/main.rs
@@ -1,6 +1,10 @@
+use futures::AsyncWriteExt;
+use futures::StreamExt;
 use sqlx::mysql::*;
 use sqlx::prelude::*;
 use std::env;
+use tokio::fs::File;
+use tokio::prelude::*;
 
 #[derive(sqlx::FromRow)]
 struct Candle {
@@ -27,12 +31,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{}", unix);
     }
 
-    // AHHHHHHH
-    // let mut stream =
-    //     sqlx::query_as::<_, Candle>("select unix from candle.binance_btc_usdt").fetch(&pool);
-    // for candle in stream.next().await? {
-    //     println!("{}", candle.unix);
-    // }
+    // Map into user defined type
+    let mut stream = sqlx::query("select unix from candle.binance_btc_usdt")
+        .map(|row: sqlx::mysql::MySqlRow| {
+            let unix: i32 = row.get(0);
+            unix
+        })
+        .fetch(&pool);
+
+    // We need to use futures::StreamExto use the `next` method
+    while let Some(record) = stream.next().await {
+        println!("{}", record.unwrap());
+    }
+
+    //
+    let mut stream =
+        sqlx::query_as::<_, Candle>("select unix from candle.binance_btc_usdt").fetch(&pool);
+    // We need to use futures::StreamExt to use the `next` method again
+    while let Some(candle) = stream.next().await {
+        println!("woot {}", candle?.unix);
+    }
+
+    // construct a file and we will stream shit into it!
+    let mut file = File::create("foo.txt").await?;
+    let mut stream =
+        sqlx::query_as::<_, Candle>("select unix from candle.binance_btc_usdt").fetch(&pool);
+    while let Some(candle) = stream.next().await {
+        let string: String = format!("wrote {}\n", candle?.unix);
+        file.write(string.as_bytes()).await?;
+    }
 
     Ok(())
 }
