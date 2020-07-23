565336e5724fa1901305e03f4635fd511b79c439
Brian Mancini <bmancini@gmail.com>
2020-07-23 08:06:05 -0400

    adding basic fs read

--- rustbook/minigrep/src/main.rs
+++ rustbook/minigrep/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // extract the arguments supplied to the program
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
@@ -8,4 +10,8 @@ fn main() {
 
     println!("Search for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text: {}", contents);
 }
