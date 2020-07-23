ec3faaac75cf1a8352ed3b38aab73fe42f1dfd78
Brian Mancini <bmancini@gmail.com>
2020-07-23 08:22:55 -0400

    refactoring minigrep to use struct

--- rustbook/minigrep/src/main.rs
+++ rustbook/minigrep/src/main.rs
@@ -2,16 +2,27 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // extract the arguments supplied to the program
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text: {}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
