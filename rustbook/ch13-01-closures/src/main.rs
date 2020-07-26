6ee87ed99d516dcf08e3e3dbfc01f2377649decc
Brian Mancini <bmancini@gmail.com>
2020-07-25 22:21:43 -0400

    creating generic memoizer

--- rustbook/ch13-01-closures/src/main.rs
+++ rustbook/ch13-01-closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::thread;
 use std::time::Duration;
 
@@ -6,10 +8,19 @@ fn main() {
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    let mut m = Memo::new(|num: i32| {
+        println!("calc");
+        num * 2
+    });
+    println!("{}", m.value(1));
+    println!("{}", m.value(1));
+    println!("{}", m.value(2));
+    println!("{}", m.value(2));
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_result = Cacher::new(|num| {
+    let mut expensive_result = Memo::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
@@ -56,6 +67,38 @@ where
                 self.value = Some(v);
                 v
             }
+        }
+    }
+}
+
+struct Memo<T, U>
+where
+    T: Fn(U) -> U,
+    U: Clone + Copy + Eq + Hash,
+{
+    func: T,
+    values: HashMap<U, U>,
+}
+
+impl<T, U> Memo<T, U>
+where
+    T: Fn(U) -> U,
+    U: Clone + Copy + Eq + Hash,
+{
+    fn new(func: T) -> Memo<T, U> {
+        Memo {
+            func,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: U) -> U {
+        if self.values.contains_key(&arg) {
+            return self.values.get(&arg).unwrap().to_owned();
+        } else {
+            let val = (self.func)(arg);
+            self.values.insert(arg, val);
+            return val;
         }
     }
 }
