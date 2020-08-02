5066c11a696946a9aad15d4e5ce0f1648c9e4843
Brian Mancini <bmancini@gmail.com>
2020-08-02 08:18:34 -0400

    adding searching via tdd

--- rustbook/minigrep/src/lib.rs
+++ rustbook/minigrep/src/lib.rs
@@ -20,12 +20,39 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Search for {}", config.query);
-    println!("In file {}", config.filename);
-
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text: {}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            matches.push(line);
+        }
+    }
+
+    matches
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
