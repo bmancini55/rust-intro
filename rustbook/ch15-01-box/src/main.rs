ffc36e851d4e827dc3fc0d2db2c27e0c00b97566
Brian Mancini <bmancini@gmail.com>
2020-08-08 16:40:46 -0400

    adding a linked list example

--- rustbook/ch15-01-box/src/main.rs
+++ rustbook/ch15-01-box/src/main.rs
@@ -6,6 +6,13 @@ fn main() {
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list: {:?}", list);
+
+    // Creates an actual linked list
+    let mut list = LinkedList::new(1);
+    LinkedList::add(&mut list, 2);
+    LinkedList::add(&mut list, 3);
+    println!("list: {:?}", list);
+    println!("last: {:?}", LinkedList::find_end(&list))
 }
 
 #[derive(Debug)]
@@ -13,3 +20,43 @@ enum List {
     Cons(i32, Box<List>),
     Nil,
 }
+
+#[derive(Debug)]
+struct LinkedList<T> {
+    value: T,
+    next: Option<Box<LinkedList<T>>>,
+}
+
+impl<T> LinkedList<T> {
+    fn new(val: T) -> Box<LinkedList<T>> {
+        Box::new(LinkedList {
+            value: val,
+            next: None,
+        })
+    }
+    fn add(list: &mut Box<LinkedList<T>>, val: T) {
+        let last = LinkedList::find_end_mut(list);
+        last.next = Some(Box::new(LinkedList {
+            value: val,
+            next: None,
+        }))
+    }
+    fn find_end(list: &Box<LinkedList<T>>) -> &Box<LinkedList<T>> {
+        let mut last = list;
+        while last.next.is_some() {
+            last = last.next.as_ref().unwrap();
+        }
+        last
+    }
+
+    fn find_end_mut(list: &mut Box<LinkedList<T>>) -> &mut Box<LinkedList<T>> {
+        let mut last = list;
+        while last.next.is_some() {
+            last = last.next.as_mut().unwrap();
+        }
+        last
+    }
+}
+
+// Possible fun exercise...
+// https://codereview.stackexchange.com/questions/204183/doubly-linked-list-in-rust
