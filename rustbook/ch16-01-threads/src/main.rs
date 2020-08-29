441110162f93464768827db20f38f09be8e79081
Brian Mancini <bmancini@gmail.com>
2020-08-29 17:46:38 -0400

    adding ch16-01-threading

---
+++ rustbook/ch16-01-threads/src/main.rs
@@ -0,0 +1,33 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // First example shows how we can spawn a thread. The thread will
+    // end when the main thread finishes. We use .join() to pause
+    // execution of the main thread until the other thread has finished
+    {
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+        handle.join().unwrap();
+    }
+
+    {
+        let v = vec![1, 2, 3];
+
+        let handle = thread::spawn(move || {
+            println!("Here's a vector: {:?}", v);
+        });
+
+        handle.join().unwrap();
+    }
+}
